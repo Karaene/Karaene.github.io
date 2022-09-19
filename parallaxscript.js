4f4880598416bf9bee645242a58dcb21386abf13
Karaene <lisa.ziener@gmail.com>
2022-09-19 13:40:03 +0200

    Letzte Änderungen
    Parallaxeffekt, Größen, Impressum

--- parallaxscript.js
+++ parallaxscript.js
@@ -8,13 +8,14 @@ let mountains_front = document.getElementById('mountains_front');
 
 window.addEventListener('scroll', function(){
   let value = window.scrollY;
+  var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight;
   stars.style.left = value * 0.25 + 'px';
   moon.style.top = value * 0.95 + 'px';
   mountains_behind.style.top = value * 0.5 + 'px';
   mountains_front.style.top = value * 0.5 + 'px';
-  text.style.marginRight = value * 2 + 'px';
+  text.style.marginRight = 51 * (value/limit) + '%';
   text.style.marginTop = value * 1.5 + 'px';
-  text2.style.marginRight = value * 2 + 'px';
+  text2.style.marginRight = 51 * (value/limit) + '%';
   text2.style.marginTop = value * 1.5 + 'px';
   btn.style.marginTop = value * 1.5 + 'px';
 })
